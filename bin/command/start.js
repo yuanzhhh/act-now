3042a8fa9a400b8d2026fe5d57d11d140d2cade7
Evan <evan.yuanzh@gmail.com>
2021-02-10 17:51:12 +0800

    -mfeat: opt

--- bin/command/start.js
+++ bin/command/start.js
@@ -1,13 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const dev_server_1 = require("../server/dev-server");
-// const devServer = require('../server/dev-server');
 const start = (next) => (program) => {
     program
         .command('start <project>')
         .description('Development Mode - Start project; 开发模式-启动项目')
         .action((_project) => {
-        // const cwd = process.cwd();
         dev_server_1.default({
             env: 'development',
         });
--- src/command/start.ts
+++ src/command/start.ts
@@ -3,15 +3,11 @@ import { Command } from 'commander';
 
 import devServer from '../server/dev-server';
 
-// const devServer = require('../server/dev-server');
-
 const start = (next: UnknownFun) => (program: Command) => {
   program
     .command('start <project>')
     .description('Development Mode - Start project; 开发模式-启动项目')
     .action((_project) => {
-      // const cwd = process.cwd();
-
       devServer({
         env: 'development',
       });
